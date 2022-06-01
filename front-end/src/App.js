9b0b2a40289ffc163f052d8859bb84c6fbe4db9f
arnoldodany44 <arnoldovaldez@gmail.com>
2022-06-01 10:09:49 -0600

    Axios and Bulma implement

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,22 @@
 // A component in React is a function
+import { Route, Switch } from 'react-router-dom';
 import NavItem from "./components/NavItem";
 import BodyItem from "./components/BodyItem";
 import BottomItem from "./components/BottomItem";
 import EndpointItem from "./components/EndpointItem";
+import Login from "./components/molecules/Login";
+import 'bulma/css/bulma.min.css';
 // Better to have just the ones used AND in a component lol
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap/js/src/collapse.js';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 // JSX with just one parent
 
   return (
     <div>
-      <NavItem />
-      <BodyItem />    
-      <EndpointItem />
-      <BottomItem /> 
+      <Switch>
+      <Route path="/login" component={Login} exact/>
+      </Switch>
     </div>
   );
 }
