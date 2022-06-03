ad1c364038f331fa528ebe10a99f449da0417cb4
arnoldodany44 <arnoldovaldez@gmail.com>
2022-06-03 12:42:38 -0600

    Add almost finished navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from "./components/molecules/Login";
 import NewUser from "./pages/NewUser";
 import Users from "./pages/Users";
+import Navbar from "./components/molecules/Navbar";
 import 'bulma/css/bulma.min.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/Api/login" component={Login} exact/>
       <Route path="/Api/user" component={NewUser} exact/>
       <Route path="/Api/users" component={Users} exact/>
+      <Route path="/navbar" component={Navbar} exact />
       </Switch>
     </div>
   );
