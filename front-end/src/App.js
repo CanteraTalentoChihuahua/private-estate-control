01b1e60106364453147b6f5ea855e1f4e8dc766a
Karol Gutierrez <karol.gutierrez@syscom.mx>
2022-06-03 12:43:54 -0600

    add almost finish sidebar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 import Login from "./components/molecules/Login";
 import NewUser from "./pages/NewUser";
 import Users from "./pages/Users";
+import Sidebar from "./components/molecules/Sidebar";
 import 'bulma/css/bulma.min.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Route path="/Api/login" component={Login} exact/>
       <Route path="/Api/user" component={NewUser} exact/>
       <Route path="/Api/users" component={Users} exact/>
+      <Route path="/Api/sidebar" component={Sidebar} exact/>
       </Switch>
     </div>
   );
