3fe307765dfa8c480899a9c643a7fe0a74a2a433
Karol Gutierrez <karol.gutierrez@syscom.mx>
2022-06-06 12:54:34 -0600

    add font awesome library

--- front-end/src/components/molecules/Sidebar.js
+++ front-end/src/components/molecules/Sidebar.js
@@ -2,6 +2,8 @@
 import React, {Component} from "react"
 //import axios from "axios"
 import './Sidebar.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faMoneyCheckAlt, faSwimmer, faUsers, faHome} from '@fortawesome/free-solid-svg-icons';
 
 export default class Users extends Component {
     
@@ -9,73 +11,107 @@ export default class Users extends Component {
         return(
             <div id="app">
 
-                <section class="main-content columns is-fullheight">
+        <nav class="navbar has-shadow">
+            <div class="container">
+                <div class="navbar-left">
+                <a class="navbar-item">
+                    <h1 class="title is-3">Website</h1>
+                </a>
+            </div>
+            <label for="menu-toggle" class="navbar-toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+            <input type="checkbox" id="menu-toggle" class="is-hidden"/>
+            <div class="navbar-end navbar-menu">
+                <a class="navbar-item is-tab is-hidden-tablet">
+                    <span class="icon"><i class="fa fa-home"></i></span> Home
+                </a>
+                <a class="navbar-item is-tab is-hidden-tablet">
+                    <span class="icon"><i class="fa fa-table"></i></span> Links
+                </a>
+                <a class="navbar-item is-tab is-hidden-tablet">
+                    <span class="icon"><i class="fa fa-info"></i></span> About
+                </a>
                 
-                <aside class="column is-3 is-narrow-mobile is-fullheight section is-hidden-mobile">
-                    <p class="menu-label is-hidden-touch">Dashboard</p>
-                    <ul class="menu-list">
+                <a class="navbar-item is-tab is-active">
+                    <span class="icon"><i class="fa fa-user"></i></span>
+                </a>
+                <a class="navbar-item is-tab">
+                    <span class="icon"><i class="fa fa-sign-out"></i></span>
+                </a>
+            </div>
+            </div>
+        </nav>
+
+        <section class="main-content columns is-fullheight">
+        
+            <aside class="column is-3 is-narrow-mobile is-fullheight section is-hidden-mobile">
+                <p class="menu-label is-hidden-touch">Dashboard</p>
+                <ul class="menu-list">
                     <li>
                         <a href="#" class="">
-                        <span class="icon"><i class="fa fa-home"></i></span> Transactions
+                            <span class="icon"><FontAwesomeIcon icon={faMoneyCheckAlt}/></span> Transactions
                         </a>
                     </li>
                     <li>
                         <a href="#" class="is-active">
-                        <span class="icon"><i class="fa-solid fa-bench-tree"></i></span> Green Areas
+                            <span class="icon"><FontAwesomeIcon icon={faSwimmer}/></span> Green Areas
                         </a>
 
                         <ul>
-                        <li>
-                            <a href="#">
-                            <span class="icon is-small"><i class="fa fa-link"></i></span> Register
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                            <span class="icon is-small"><i class="fa fa-link"></i></span> Edit
-                            </a>
-                        </li>
+                            <li>
+                                <a href="#">
+                                    <span class="icon is-small"><i class="fa fa-link"></i></span> Register
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <span class="icon is-small"><i class="fa fa-link"></i></span> Edit
+                                </a>
+                            </li>
                         </ul>
                     </li>
                     <li>
                         <a href="#" class="">
-                        <span class="icon"><i class="fa fa-info"></i></span> Users
+                            <span class="icon"><FontAwesomeIcon icon={faUsers}/></span> Users
                         </a>
                     </li>
                     <li>
                         <a href="#" class="">
-                        <span class="icon"><i class="fa fa-info"></i></span> Houses
+                            <span class="icon"><FontAwesomeIcon icon={faHome}/></span> Houses
                         </a>
                     </li>
                     <li>
                         <a href="#" class="">
-                        <span class="icon"><i class="fa fa-info"></i></span> Requests
+                            <span class="icon"><i class="fa fa-info"></i></span> Requests
                         </a>
                     </li>
                     <li>
                         <a href="#" class="">
-                        <span class="icon"><i class="fa fa-info"></i></span> Entries
+                            <span class="icon"><i class="fa fa-info"></i></span> Entries
                         </a>
                     </li>
                     <li>
                         <a href="#" class="">
-                        <span class="icon"><i class="fa fa-info"></i></span> Roles
+                            <span class="icon"><i class="fa fa-info"></i></span> Roles
                         </a>
                     </li>
                     <li>
                         <a href="#" class="">
-                        <span class="icon"><i class="fa fa-info"></i></span> Vehicles
+                            <span class="icon"><i class="fa fa-info"></i></span> Vehicles
                         </a>
                     </li>
                     <li>
                         <a href="#" class="">
-                        <span class="icon"><i class="fa fa-info"></i></span> Visitors
+                            <span class="icon"><i class="fa fa-info"></i></span> Visitors
                         </a>
                     </li>
-                    </ul>
-                </aside>
+                </ul>
+            </aside>
                 
-                </section>
+        </section>
                 
             </div>
         )
