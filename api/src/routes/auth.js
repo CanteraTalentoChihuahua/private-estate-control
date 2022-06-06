9efb082c40bd1f34d443f8adc493a01eb6ea4993
Pwma77 <brayanpaulsalas.77@gmail.com>
2022-06-06 09:53:37 -0600

    branches check

--- api/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../controllers/auth");
-const router = (0, express_1.Router)();
-router.get('/test', auth_1.validateToken, auth_1.testGet);
-router.get('/', auth_1.getLogin);
-router.post('/auth', auth_1.authLogin);
-exports.default = router;
