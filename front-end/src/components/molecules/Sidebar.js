45fa2d3c8295af80e9aa41c719bface91f863b97
Karol Gutierrez <karol.gutierrez@syscom.mx>
2022-06-07 12:31:55 -0600

    avances sidebar

--- front-end/src/components/molecules/Sidebar.js
+++ front-end/src/components/molecules/Sidebar.js
@@ -3,7 +3,8 @@ import React, {Component} from "react"
 //import axios from "axios"
 import './Sidebar.css';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMoneyCheckAlt, faSwimmer, faUsers, faHome, faExclamation, faCar, faChild, faPlus, faEdit} from '@fortawesome/free-solid-svg-icons';
+import {faMoneyCheckAlt, faSwimmer, faUsers, faHome, faCar, faChild, faPlus, faEdit, faLockOpen, faUserTag, faUserCircle, faPaperPlane, faSignOutAlt, faChartBar} from '@fortawesome/free-solid-svg-icons';
+import { click } from "@testing-library/user-event/dist/click";
 
 export default class Users extends Component {
     
@@ -13,17 +14,15 @@ export default class Users extends Component {
 
         <nav class="navbar has-shadow">
             <div class="container">
-                <div class="navbar-left">
-                <a class="navbar-item">
-                    <h1 class="title is-3">Website</h1>
-                </a>
+                <div class="navbar-left container">
+                    <h1 class="title is-3 navbar-item">MIND</h1>
             </div>
-            <label for="menu-toggle" class="navbar-toggle">
+            <label for="navbar-menu" class="navbar-toggle">
                 <span></span>
                 <span></span>
                 <span></span>
             </label>
-            <input type="checkbox" id="menu-toggle" class="is-hidden"/>
+            <input type="checkbox" id="navbar-menu" class="is-hidden"/>
             <div class="navbar-end navbar-menu">
                 <a class="navbar-item is-tab is-hidden-tablet">
                     <span class="icon"><i class="fa fa-home"></i></span> Home
@@ -35,11 +34,11 @@ export default class Users extends Component {
                     <span class="icon"><i class="fa fa-info"></i></span> About
                 </a>
                 
-                <a class="navbar-item is-tab is-active">
-                    <span class="icon"><i class="fa fa-user"></i></span>
+                <a class="navbar-item is-tab">
+                    <span class="icon"><FontAwesomeIcon icon={faUserCircle}/></span>
                 </a>
                 <a class="navbar-item is-tab">
-                    <span class="icon"><i class="fa fa-sign-out"></i></span>
+                    <span class="icon"><FontAwesomeIcon icon={faSignOutAlt}/></span>
                 </a>
             </div>
             </div>
@@ -47,16 +46,20 @@ export default class Users extends Component {
 
         <section class="main-content columns is-fullheight">
         
-            <aside class="column is-3 is-narrow-mobile is-fullheight section is-hidden-mobile">
-                <p class="menu-label is-hidden-touch">Dashboard</p>
+            <aside class="column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile">
                 <ul class="menu-list">
+                    <li>
+                        <a href="#" class="">
+                            <span class="icon"><FontAwesomeIcon icon={faChartBar}/></span> Dashboard
+                        </a>
+                    </li>
                     <li>
                         <a href="#" class="">
                             <span class="icon"><FontAwesomeIcon icon={faMoneyCheckAlt}/></span> Transactions
                         </a>
                     </li>
-                    <li>
-                        <a href="#" class="is-active">
+                    {/*<li>
+                        <a href="#">
                             <span class="icon"><FontAwesomeIcon icon={faSwimmer}/></span> Green Areas
                         </a>
 
@@ -72,7 +75,7 @@ export default class Users extends Component {
                                 </a>
                             </li>
                         </ul>
-                    </li>
+                    </li>*/}
                     <li>
                         <a href="#" class="">
                             <span class="icon"><FontAwesomeIcon icon={faUsers}/></span> Users
@@ -83,19 +86,19 @@ export default class Users extends Component {
                             <span class="icon"><FontAwesomeIcon icon={faHome}/></span> Houses
                         </a>
                     </li>
+                    {/*<li>
+                        <a href="#" class="">
+                            <span class="icon"><FontAwesomeIcon icon={faPaperPlane}/></span> Requests
+                        </a>
+                    </li>*/}
                     <li>
                         <a href="#" class="">
-                            <span class="icon"><FontAwesomeIcon icon={faExclamation}/></span> Requests
+                            <span class="icon"><FontAwesomeIcon icon={faLockOpen}/></span> Entries
                         </a>
                     </li>
-                    <li>
+                    {/*<li>
                         <a href="#" class="">
-                            <span class="icon"><i class="fa fa-info"></i></span> Entries
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="">
-                            <span class="icon"><i class="fa fa-info"></i></span> Roles
+                            <span class="icon"><FontAwesomeIcon icon={faUserTag}/></span> Roles
                         </a>
                     </li>
                     <li>
@@ -107,7 +110,7 @@ export default class Users extends Component {
                         <a href="#" class="">
                             <span class="icon"><FontAwesomeIcon icon={faChild}/></span> Visitors
                         </a>
-                    </li>
+                    </li>*/}
                 </ul>
             </aside>
                 
