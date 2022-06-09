55d56c6345ad75dd06b2d5f23d2bde62f69d9af8
Arnoldodany <a311006@uach.mx>
2022-06-08 23:00:26 -0600

    Add navbar and sidebar to users page

--- front-end/src/pages/Users.js
+++ front-end/src/pages/Users.js
@@ -1,7 +1,9 @@
 //import { Link } from "react-router-dom"
 import axios from "axios"
 import React, {Component} from "react"
-
+import Title from "../components/atoms/Title";
+import Navbar from "../components/molecules/Navbar"
+import Sidebar from "../components/molecules/Sidebar"
 export default class Users extends Component {
     state={
         users:[]
@@ -21,35 +23,38 @@ export default class Users extends Component {
     }
     render() {
         return(
-    <section className="hero is-primary is-fullheight is-link">
-    <div className="hero-body">
-      <div className="container">
-        <div className="columns is-centered">
-          <div className="column is-5-tablet is-4-desktop is-5-widescreen">
-          <h1 className="title is-2">Users</h1>
-            <table className="table is-striped">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Lastname</th>
-                        <th>Phone Number</th>
-                        <th>Email</th>
-                        <th>Active</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.users.map(user => <tr key={user.id} onDoubleClick={()=> this.deleteUser(user.IdUser)}>
-                            <td>{user.FirstName}</td><td>{user.LastName}</td><td>{user.PhoneNumber}</td><td>{user.Email}</td><td>{user.Active}</td>
-                            </tr>)
-                    }
-                </tbody>
-            </table> 
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+            <div>
+            <Navbar/>
+                <section className="main-content columns is-fullheight">
+                    <Sidebar/>
+                    <div className="container column is-10">
+                        <br/>
+                        <section className="hero is-primary is-fullheight is-link">
+                        <div className="column is-5-tablet is-4-desktop is-5-widescreen">
+                            <Title title="Users"/>
+                            <table className="table is-striped">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Lastname</th>
+                                        <th>Phone Number</th>
+                                        <th>Email</th>
+                                        <th>Active</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    this.state.users.map(user => <tr key={user.id} onDoubleClick={()=> this.deleteUser(user.IdUser)}>
+                                    <td>{user.FirstName}</td><td>{user.LastName}</td><td>{user.PhoneNumber}</td><td>{user.Email}</td><td>{user.Active}</td>
+                                    </tr>)
+                                }
+                                </tbody>
+                            </table> 
+                        </div>
+                        </section>
+                    </div>
+                </section>
+                </div>
         )
     }
 }
