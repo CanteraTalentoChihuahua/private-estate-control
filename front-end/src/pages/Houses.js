5c5d8861eb58d11adb48ebe885ee8c38c85f029b
Karol Gutierrez <karol.gutierrez@syscom.mx>
2022-06-17 12:39:10 -0600

    crud houses

--- front-end/src/pages/Houses.js
+++ front-end/src/pages/Houses.js
@@ -1,8 +1,21 @@
+import axios from "axios"
 import React, {Component} from "react";
 import Sidebar from "../components/molecules/Sidebar";
 import Title from "../components/atoms/Title";
 import Navbar from "../components/molecules/Navbar";
 export default class Houses extends Component {
+    state={
+        houses:[{Address: "Provincia del Lago #921", Balance: "$300.00"},{Address: "Av. Colegio Militar #470", Balance: "$520.00"}]
+    }
+    async getHouses(){
+        const res = await axios.get('https://gestion-fraccionamiento.herokuapp.com/houses/get');
+        this.setState({houses:res.data});
+        console.log(res.data);
+    }
+    async componentDidMount(){
+        this.getHouses();
+        console.log(this.state.houses);
+    }
     render() {
         const tkn = JSON.parse(localStorage.getItem('tkn'));
         if(tkn==null){
@@ -15,7 +28,26 @@ export default class Houses extends Component {
                     <Sidebar/>
                     <div className="container column is-10 m-0">
                         <br/>
-                        <Title title="Houses"/>
+                        <section className="is-fullheight">
+                        <div className="column is-5-tablet is-4-desktop is-5-widescreen">
+                            <Title title="Houses"/>
+                            <table className="table is-striped is-fullwidth">
+                                <thead>
+                                    <tr>
+                                        <th>Address</th>
+                                        <th>Balance</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                {
+                                    this.state.houses.map(house => <tr key={house.IdHouse}>
+                                    <td>{house.Address}</td><td>{house.Balance}</td>
+                                    </tr>)
+                                }
+                                </tbody>
+                            </table>
+                        </div>
+                        </section>
                     </div>
                 </section>
             </div>
