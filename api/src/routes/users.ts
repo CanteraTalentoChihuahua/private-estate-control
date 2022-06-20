f40c123ecc67e3164c306f805a4628d3ba1081cf
Pwma77 <56004648+Pwma77@users.noreply.github.com>
2022-06-20 00:23:48 -0500

    Minimal fixes & Outcomes x Incomes postman tested

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -8,12 +8,12 @@ router.get("/get", validateSA, getUsers);
 
 router.get("/get/count", validateToken, getTotalUsers);
 
-router.get("/get/:id", validateToken, getUserById);
+router.get("/get/:id", getUserById);
 
 router.post("/post", createUser);
 
-router.put("/put/:id", validateToken, updateUserById);
+router.put("/put/:id", updateUserById);
 
-router.delete("/delete/:id", validateToken, deleteUserById);
+router.delete("/delete/:id", deleteUserById);
 
 export default router;
