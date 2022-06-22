8924003e4d40c5d8604bfe2c92cfa6c49b1d855d
Pwma77 <brayanpaulsalas.77@gmail.com>
2022-06-22 09:29:10 -0600

    Phone number added in reports

--- api/src/models/reports.ts
+++ api/src/models/reports.ts
@@ -1,7 +1,7 @@
 export default {
     getAllUsersHouses: `SELECT * FROM T_UsersHouses`,
     createUsersHousesRegister: `INSERT INTO T_UsersHouses(IdHouse, IdUser) VALUES (@idHouse, @idUser)`,
-    reportUsersHouses: `SELECT T_Users.IdUser, T_Users.FirstName, T_Users.LastName, T_Users.Email, T_Users.Active, T_Users.FaceID,
+    reportUsersHouses: `SELECT T_Users.IdUser, T_Users.FirstName, T_Users.LastName, T_Users.PhoneNumber, T_Users.Email, T_Users.Active, T_Users.FaceID,
     T_Houses.IdHouse, T_Houses.Address, T_Houses.Balance, T_Houses.Occuppied, T_Houses.IdResDev, T_UsersHouses.IdUserHouse
     FROM T_Users
     FULL OUTER JOIN T_UsersHouses
